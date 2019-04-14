d8341c1752335e2a1a206f472db15e54bc0a8f6c
tianhaoz95 <tianhaoz@umich.edu>
2019-04-14 16:38:46 -0700

    init the bare bone language checker

---
+++ projects/grammar-check/README.md
@@ -0,0 +1,1 @@
+# The grammar check automation
---
+++ projects/grammar-check/index.js
@@ -0,0 +1,46 @@
+var fs = require('fs');
+var path = require('path');
+var argument_parser = require('minimist');
+
+var walk = (dir, done) => {
+    var results = [];
+    fs.readdir(dir, (err, list) => {
+        if (err) {
+            return done(err);
+        }
+        var pending = list.length;
+        if (!pending) {
+            return done(null, results);
+        }
+        list.forEach((file) => {
+            file = path.resolve(dir, file);
+            fs.stat(file, (err, stat) => {
+                if (stat && stat.isDirectory()) {
+                    walk(file, (err, res) => {
+                        results = results.concat(res);
+                        pending = pending - 1;
+                        if (!pending) {
+                            done(null, results);
+                        }
+                    });
+                } else {
+                    results.push(file);
+                    pending = pending - 1;
+                    if (!pending) {
+                        done(null, results);
+                    }
+                }
+            });
+        });
+    })
+}
+
+var entry_point = () => {
+    var arguments = argument_parser(process.argv.slice(2));
+    walk('./', (err, results) => {
+        if (err) throw err;
+        console.log(results);
+    });
+}
+
+entry_point();
