01da03a7bb5324d8da13e27b4f64d6b40a01d42e
Tianhao Zhou <tianhaoz@umich.edu>
2019-07-28 23:52:36 +0000

    wip

--- projects/lint/index.js
+++ projects/lint/index.js
@@ -1,2 +1,37 @@
 const Octokit = require('@octokit/rest')
-const octokit = new Octokit()
+const octokit = new Octokit({
+    auth: process.env.GITHUB_TOKEN
+})
+const writeGood = require('write-good')
+var fs = require('fs')
+
+lint_lists = [
+    {
+        id: 'cool gadgets cover page',
+        path: '../../cool-gadgets/README.md'
+    }
+]
+
+octokit.issues.create({
+  owner: 'tianhaoz95',
+  repo: 'developer-note',
+  title: 'test',
+  body: 'test 1'
+})
+.then((res) => {
+    console.log(res)
+})
+.catch((err) => {
+    console.log('error!')
+    console.log(err)
+})
+
+fs.readFile('../../README.md', 'utf8', function(err, contents) {
+    console.log(contents);
+})
+
+function lint_file(file_list) {
+    return new Promise((resolve, reject) => {
+
+    })
+}
